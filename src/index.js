e8a9fef4bc33407e2b5a5ccbd3d6018a64cd4ce2
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-02-23 17:58:29 +0900

    개오 - React란?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 class Square extends React.Component {
   render() {
     return <button className="square">{/* TODO */}</button>;
